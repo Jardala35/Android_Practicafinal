35a9fc5edce17a86b05fd65f213230aa76c86150
Pablo Navarro <pablonavarroduro@gmail.com>
2023-11-28 16:17:44 +0100

    Estadisticas terminado

--- app/src/main/java/com/example/practica_final_grupo9_sergioypablo/Estadisticas.java
+++ app/src/main/java/com/example/practica_final_grupo9_sergioypablo/Estadisticas.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -14,9 +15,10 @@ public class Estadisticas extends AppCompatActivity {
 
     private ImageView[] imgs;
     private Button[] butons;
-
     private int iter;
     private int[] est;
+
+    private EditText[] listaetxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +35,14 @@ public class Estadisticas extends AppCompatActivity {
         butons[3] = findViewById(R.id.btn4);
         butons[4] = findViewById(R.id.btn5);
         butons[5] = findViewById(R.id.btn6);
+        listaetxt = new EditText[6];
+        listaetxt[0] = findViewById(R.id.etxt1);
+        listaetxt[1] = findViewById(R.id.etxt2);
+        listaetxt[2] = findViewById(R.id.etxt3);
+        listaetxt[3] = findViewById(R.id.etxt4);
+        listaetxt[4] = findViewById(R.id.etxt5);
+        listaetxt[5] = findViewById(R.id.etxt6);
+
         this.iter = 0;
 
 
@@ -41,12 +51,39 @@ public class Estadisticas extends AppCompatActivity {
 
     public void generar(View view){
         LinearLayout ln = findViewById(R.id.layimg);
+
         if(this.iter == 0){
             ln.setVisibility(View.VISIBLE);
         }
-        this.est[iter] = dados(view, this.imgs);
+
+
+        if(view.getId() == R.id.btn1){
+            this.est[0] = dados(view, this.imgs);
+            this.listaetxt[0].setText(Integer.toString(this.est[0]));
+        }
+        if(view.getId() == R.id.btn2){
+            this.est[1] = dados(view, this.imgs);
+            this.listaetxt[1].setText(Integer.toString(this.est[1]));
+        }
+        if(view.getId() == R.id.btn3){
+            this.est[2] = dados(view, this.imgs);
+            this.listaetxt[2].setText(Integer.toString(this.est[2]));
+        }
+        if(view.getId() == R.id.btn4){
+            this.est[3] = dados(view, this.imgs);
+            this.listaetxt[3].setText(Integer.toString(this.est[3]));
+        }
+        if(view.getId() == R.id.btn5){
+            this.est[4] = dados(view, this.imgs);
+            this.listaetxt[4].setText(Integer.toString(this.est[4]));
+        }
+        if(view.getId() == R.id.btn6){
+            this.est[5] = dados(view, this.imgs);
+            this.listaetxt[5].setText(Integer.toString(this.est[5]));
+        }
         iter++;
         view.setVisibility(View.INVISIBLE);
+
         if (iter == 5) {
             new Handler().postDelayed(new Runnable() {
                 @Override
